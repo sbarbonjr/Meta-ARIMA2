74410a6bdfb5e4a7aa035b21c1018e818527875b
sbarbonjr <sbarbonjr@gmail.com>
2020-09-01 19:00:16 -0300

    Update All_2.py

--- All_2.py
+++ All_2.py
@@ -70,7 +70,7 @@ series = pd.read_csv(url, sep=",", squeeze=True)
 series = pd.to_numeric(series, errors='coerce')
 series = pd.Series(np.nan_to_num(series))
 
-series[10000:20000]
+series = series[10000:20000]
 output_file = "dataset_2.csv"
 
 
