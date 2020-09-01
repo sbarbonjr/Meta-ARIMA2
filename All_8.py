c5e6d42b5ea31af95621961aea0e7fa6afe673d9
sbarbonjr <sbarbonjr@gmail.com>
2020-09-01 19:03:01 -0300

    Update All_8.py

--- All_8.py
+++ All_8.py
@@ -70,7 +70,7 @@ series = pd.read_csv(url, sep=",", squeeze=True)
 series = pd.to_numeric(series, errors='coerce')
 series = pd.Series(np.nan_to_num(series))
 
-series[80000:90000]
+series = series[80000:90000]
 output_file = "dataset_8.csv"
 
 
