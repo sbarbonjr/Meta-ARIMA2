c4a35eb2925cda31e22eee80fa9c84ce726762a2
sbarbonjr <sbarbonjr@gmail.com>
2020-09-01 19:02:10 -0300

    Update All_6.py

--- All_6.py
+++ All_6.py
@@ -70,7 +70,7 @@ series = pd.read_csv(url, sep=",", squeeze=True)
 series = pd.to_numeric(series, errors='coerce')
 series = pd.Series(np.nan_to_num(series))
 
-series[60000:70000]
+series = series[60000:70000]
 output_file = "dataset_6.csv"
 
 
